57b41647ee09c7883f7b79423a405182e586f934
ghost2468developer <siyabonga.ncube669@gmail.com>
2024-12-30 15:41:41 +0200

    task testing

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -3,20 +3,17 @@ import * as taskService from '../services/taskService'
 
 const router = express.Router()
 
-// Create a task
 router.post('/tasks', async (req, res) => {
   const { name, description, status } = req.body
     const taskId = await taskService.createTask(name, description, status)
     res.status(201).json({ id: taskId })
 })
 
-// Get all tasks
 router.get('/tasks', async (req, res) => {
     const tasks = await taskService.getAllTasks()
     res.status(200).json(tasks)
 })
 
-// Get a task by ID
 router.get('/tasks/:id', async (req, res) => {
   const { id } = req.params
     const task = await taskService.getTaskById(Number(id))
@@ -24,7 +21,6 @@ router.get('/tasks/:id', async (req, res) => {
     else res.status(404).json({ message: 'Task not found' })
 })
 
-// Update a task
 router.put('/tasks/:id', async (req, res) => {
   const { id } = req.params
   const { name, description, status } = req.body
@@ -33,7 +29,6 @@ router.put('/tasks/:id', async (req, res) => {
     else res.status(404).json({ message: 'Task not found' })
 })
 
-// Delete a task
 router.delete('/tasks/:id', async (req, res) => {
   const { id } = req.params
     const deleted = await taskService.deleteTask(Number(id))
